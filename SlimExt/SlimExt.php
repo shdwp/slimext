a1e061dbe04e257507dee5af05d50e411001070a
ShadowPrince <shadowprince47@gmail.com>
2013-10-11 11:47:15 +0300

    update headers

--- SlimExt/SlimExt.php
+++ SlimExt/SlimExt.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * SlimExt
+ *
+ * @author Vasiliy Horbachenko <shadow.prince@ya.ru>
+ * @copyright 2013 Vasiliy Horbachenko
+ * @version 1.0
+ * @package shadowprince/slimext
+ *
+ */
 namespace SlimExt;
 
 class SlimExt extends \Slim\Slim {
